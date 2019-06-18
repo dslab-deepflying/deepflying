48daa188dc473af9336dadc45a5f7e84bd4c80c1
JC <624099049@qq.com>
2019-06-18 10:45:40 +0800

    modified

--- InnoCamp/SF/neural_style_transfer.py
+++ InnoCamp/SF/neural_style_transfer.py
@@ -1,93 +1,22 @@
-'''Neural style transfer with Keras.
-
-Run the script with:
-```
-python neural_style_transfer.py path_to_your_base_image.jpg \
-    path_to_your_reference.jpg prefix_for_results
-```
-e.g.:
-```
-python neural_style_transfer.py img/tuebingen.jpg \
-    img/starry_night.jpg results/my_result
-```
-Optional parameters:
-```
---iter, To specify the number of iterations \
-    the style transfer takes place (Default is 10)
---content_weight, The weight given to the content loss (Default is 0.025)
---style_weight, The weight given to the style loss (Default is 1.0)
---tv_weight, The weight given to the total variation loss (Default is 1.0)
-```
-
-It is preferable to run this script on GPU, for speed.
-
-Example result: https://twitter.com/fchollet/status/686631033085677568
-
-# Details
-
-Style transfer consists in generating an image
-with the same "content" as a base image, but with the
-"style" of a different picture (typically artistic).
-
-This is achieved through the optimization of a loss function
-that has 3 components: "style loss", "content loss",
-and "total variation loss":
-
-- The total variation loss imposes local spatial continuity between
-the pixels of the combination image, giving it visual coherence.
-
-- The style loss is where the deep learning keeps in --that one is defined
-using a deep convolutional neural network. Precisely, it consists in a sum of
-L2 distances between the Gram matrices of the representations of
-the base image and the style reference image, extracted from
-different layers of a convnet (trained on ImageNet). The general idea
-is to capture color/texture information at different spatial
-scales (fairly large scales --defined by the depth of the layer considered).
-
- - The content loss is a L2 distance between the features of the base
-image (extracted from a deep layer) and the features of the combination image,
-keeping the generated image close enough to the original one.
-
-# References
-    - [A Neural Algorithm of Artistic Style](http://arxiv.org/abs/1508.06576)
-'''
-
 from __future__ import print_function
 from keras.preprocessing.image import load_img, save_img, img_to_array
 import numpy as np
 from scipy.optimize import fmin_l_bfgs_b
 import time
-import argparse
 
 from keras.applications import vgg19
 from keras import backend as K
 
-parser = argparse.ArgumentParser(description='Neural style transfer with Keras.')
-parser.add_argument('base_image_path', metavar='base', type=str,
-                    help='Path to the image to transform.')
-parser.add_argument('style_reference_image_path', metavar='ref', type=str,
-                    help='Path to the style reference image.')
-parser.add_argument('result_prefix', metavar='res_prefix', type=str,
-                    help='Prefix for the saved results.')
-parser.add_argument('--iter', type=int, default=20, required=False,
-                    help='Number of iterations to run.')
-parser.add_argument('--content_weight', type=float, default=0.025, required=False,
-                    help='Content weight.')
-parser.add_argument('--style_weight', type=float, default=1.0, required=False,
-                    help='Style weight.')
-parser.add_argument('--tv_weight', type=float, default=1.0, required=False,
-                    help='Total Variation weight.')
 
-args = parser.parse_args()
-base_image_path = args.base_image_path
-style_reference_image_path = args.style_reference_image_path
-result_prefix = args.result_prefix
-iterations = args.iter
+base_image_path = "pics/"
+style_reference_image_path = "pics/"
+result_prefix = "combined"
+iterations = 20
 
 # these are the weights of the different loss components
-total_variation_weight = args.tv_weight
-style_weight = args.style_weight
-content_weight = args.content_weight
+total_variation_weight = 0.025
+style_weight = 1.0
+content_weight = 1.0
 
 # dimensions of the generated picture.
 width, height = load_img(base_image_path).size
